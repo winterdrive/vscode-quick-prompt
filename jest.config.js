1a7bd3414ffb83ba86d978dc06610154accfe1b9
Tang, Sheng-Kai <90021888+winterdrive@users.noreply.github.com>
2026-07-22 15:13:47 +0800

    test: ignore local claude worktrees in jest (#45)

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,8 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     testMatch: ['**/src/test/**/*.test.ts'],
-    testPathIgnorePatterns: ['/node_modules/', '/src/test/ui/', '/.vscode-test/'],
-    modulePathIgnorePatterns: ['<rootDir>/.vscode-test/', '<rootDir>/dist/'],
+    testPathIgnorePatterns: ['/node_modules/', '/src/test/ui/', '/.vscode-test/', '/.claude/'],
+    modulePathIgnorePatterns: ['<rootDir>/.vscode-test/', '<rootDir>/.claude/', '<rootDir>/dist/'],
     transform: {
         '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.test.json' }],
     },
